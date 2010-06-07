17ac4d336b0cf04903de07d6982645fe6a09dede
Larry Marburger <larry@marburger.cc>
2010-06-07 23:13:17 +0800

    Include lib directory when executing tests.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "\033[1;81m Running test_time_stack_item...\033[0m"
-ruby test_time_stack_item.rb || (echo "FAILED!!!!!!!!!!!!")
+ruby -I../lib test_time_stack_item.rb || (echo "FAILED!!!!!!!!!!!!")
 
 echo "\033[1;81m Running test_timecop_without_date...\033[0m"
-ruby test_timecop_without_date.rb || (echo "FAILED!!!!!!!!!!!!")
+ruby -I../lib test_timecop_without_date.rb || (echo "FAILED!!!!!!!!!!!!")
 
 echo "\033[1;81m Running test_timecop_without_date_but_with_time...\033[0m"
-ruby test_timecop_without_date_but_with_time.rb || (echo "FAILED!!!!!!!!!!!!")
+ruby -I../lib test_timecop_without_date_but_with_time.rb || (echo "FAILED!!!!!!!!!!!!")
 
 echo "\033[1;81m Running test_timecop...\033[0m"
-ruby test_timecop.rb || (echo "FAILED!!!!!!!!!!!!")
+ruby -I../lib test_timecop.rb || (echo "FAILED!!!!!!!!!!!!")
