537063c9eda27d77569a53667cdfc49bc88b117b
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2011-12-21 20:33:18 +0100

    Use echo -e to provide proper escape sequences.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "\033[1;81m Running test_time_stack_item...\033[0m"
-ruby -I../lib:. test_time_stack_item.rb || (echo "FAILED!!!!!!!!!!!!")
+echo -e "\033[1;81m Running test_time_stack_item...\033[0m"
+ruby -I../lib:. test_time_stack_item.rb || (echo -e "FAILED!!!!!!!!!!!!")
 
-echo "\033[1;81m Running test_timecop_without_date...\033[0m"
-ruby -I../lib:. test_timecop_without_date.rb || (echo "FAILED!!!!!!!!!!!!")
+echo -e "\033[1;81m Running test_timecop_without_date...\033[0m"
+ruby -I../lib:. test_timecop_without_date.rb || (echo -e "FAILED!!!!!!!!!!!!")
 
-echo "\033[1;81m Running test_timecop_without_date_but_with_time...\033[0m"
-ruby -I../lib:. test_timecop_without_date_but_with_time.rb || (echo "FAILED!!!!!!!!!!!!")
+echo -e "\033[1;81m Running test_timecop_without_date_but_with_time...\033[0m"
+ruby -I../lib:. test_timecop_without_date_but_with_time.rb || (echo -e "FAILED!!!!!!!!!!!!")
 
-echo "\033[1;81m Running test_timecop...\033[0m"
-ruby -I../lib:. test_timecop.rb || (echo "FAILED!!!!!!!!!!!!")
+echo -e "\033[1;81m Running test_timecop...\033[0m"
+ruby -I../lib:. test_timecop.rb || (echo -e "FAILED!!!!!!!!!!!!")
