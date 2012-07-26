fc81958ee8734f012f5bb14363869a88c0a6165b
Travis Jeffery <travisjeffery@gmail.com>
2012-07-26 04:36:16 +0000

    better way of getting tests to run

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-echo "\033[1;81m Running test_time_stack_item...\033[0m"
-ruby -I../lib:. test_time_stack_item.rb || (echo "FAILED!!!!!!!!!!!!")
+for f in *_test.rb; do
+  ruby -I../lib:. $f || (echo "\n$f failed :(\n------------------------------")
+done
 
-echo "\033[1;81m Running test_timecop_without_date...\033[0m"
-ruby -I../lib:. test_timecop_without_date.rb || (echo "FAILED!!!!!!!!!!!!")
-
-echo "\033[1;81m Running test_timecop_without_date_but_with_time...\033[0m"
-ruby -I../lib:. test_timecop_without_date_but_with_time.rb || (echo "FAILED!!!!!!!!!!!!")
-
-echo "\033[1;81m Running test_timecop...\033[0m"
-ruby -I../lib:. test_timecop.rb || (echo "FAILED!!!!!!!!!!!!")
