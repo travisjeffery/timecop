fe68947e256eca3fa48f4fe427edc7457c701b78
Travis Jeffery <travisjeffery@gmail.com>
2012-07-29 02:04:44 -0400

    replace hack job of a mock

--- test/time_stack_item_test.rb
+++ test/time_stack_item_test.rb
@@ -184,17 +184,7 @@ class TestTimeStackItem < Test::Unit::TestCase
 
   def test_integration_with_rails_time_zone
     time = Time.now
-    def time.in_time_zone
-      self - 3600
-    end
-    t = time - 3600
-    stack_item = Timecop::TimeStackItem.new(:freeze, time)
-    y, m, d, h, min, s = t.year, t.month, t.day, t.hour, t.min, t.sec
-    assert_equal y,   stack_item.year
-    assert_equal m,   stack_item.month
-    assert_equal d,   stack_item.day
-    assert_equal h,   stack_item.hour
-    assert_equal min, stack_item.min
-    assert_equal s,   stack_item.sec
+    Time.any_instance.expects(:in_time_zone).returns(Time.now)
+    Timecop::TimeStackItem.new(:freeze, time)
   end
 end
