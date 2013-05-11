0d5d4d5289bb5b6b353fbcaeed851e965dbc70fb
Micah Chalmer <micah@micahchalmer.net>
2013-05-11 16:49:49 -0400

    Make TimeCop work with Date/DateTime even if Timecop was loaded first

--- lib/timecop/time_extensions.rb
+++ lib/timecop/time_extensions.rb
@@ -1,3 +1,4 @@
+require 'date'
 
 class Time #:nodoc:
   class << self
@@ -24,52 +25,48 @@ class Time #:nodoc:
   end
 end 
 
-if Object.const_defined?(:Date) && Date.respond_to?(:today)
-  class Date #:nodoc:
-    class << self
-      def mock_date
-        mocked_time_stack_item = Timecop.top_stack_item
-        mocked_time_stack_item.nil? ? nil : mocked_time_stack_item.date(self)
-      end
-      
-      alias_method :today_without_mock_date, :today
-    
-      def today_with_mock_date
-        mock_date || today_without_mock_date
-      end
-    
-      alias_method :today, :today_with_mock_date
+class Date #:nodoc:
+  class << self
+    def mock_date
+      mocked_time_stack_item = Timecop.top_stack_item
+      mocked_time_stack_item.nil? ? nil : mocked_time_stack_item.date(self)
     end
+    
+    alias_method :today_without_mock_date, :today
+    
+    def today_with_mock_date
+      mock_date || today_without_mock_date
+    end
+    
+    alias_method :today, :today_with_mock_date
   end
 end
 
-if Object.const_defined?(:DateTime) && DateTime.respond_to?(:now)
-  class DateTime #:nodoc:
-    class << self
-      def mock_time
-        mocked_time_stack_item = Timecop.top_stack_item
-        mocked_time_stack_item.nil? ? nil : mocked_time_stack_item.datetime(self)
-      end
-      
-      def now_without_mock_time
-        Time.now_without_mock_time.to_datetime
-      end
-      
-      def now_with_mock_time
-        mock_time || now_without_mock_time
-      end
-
-      alias_method :now, :now_with_mock_time
+class DateTime #:nodoc:
+  class << self
+    def mock_time
+      mocked_time_stack_item = Timecop.top_stack_item
+      mocked_time_stack_item.nil? ? nil : mocked_time_stack_item.datetime(self)
     end
-  end
+    
+    def now_without_mock_time
+      Time.now_without_mock_time.to_datetime
+    end
+    
+    def now_with_mock_time
+      mock_time || now_without_mock_time
+    end
 
-  # for ruby1.8
-  unless Time::public_instance_methods.include? :to_datetime
-    class DateTime
-      class << self
-        def now_without_mock_time
-          Time.now_without_mock_time.send(:to_datetime)
-        end
+    alias_method :now, :now_with_mock_time
+  end
+end
+
+# for ruby1.8
+unless Time::public_instance_methods.include? :to_datetime
+  class DateTime
+    class << self
+      def now_without_mock_time
+        Time.now_without_mock_time.send(:to_datetime)
       end
     end
   end
