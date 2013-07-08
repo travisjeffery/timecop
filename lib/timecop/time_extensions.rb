cbafd38581431e334ad61a6d4e8305558ca58ece
Travis Jeffery <travisjeffery@gmail.com>
2013-07-08 01:09:26 -0500

    remove mock specific for 1.8

--- lib/timecop/time_extensions.rb
+++ lib/timecop/time_extensions.rb
@@ -6,13 +6,13 @@ class Time #:nodoc:
       mocked_time_stack_item = Timecop.top_stack_item
       mocked_time_stack_item.nil? ? nil : mocked_time_stack_item.time(self)
     end
-    
+
     alias_method :now_without_mock_time, :now
 
     def now_with_mock_time
       mock_time || now_without_mock_time
     end
-    
+
     alias_method :now, :now_with_mock_time
 
     alias_method :new_without_mock_time, :new
@@ -20,10 +20,10 @@ class Time #:nodoc:
     def new_with_mock_time(*args)
       args.size <= 0 ? now : new_without_mock_time(*args)
     end
-    
+
     alias_method :new, :new_with_mock_time
   end
-end 
+end
 
 class Date #:nodoc:
   class << self
@@ -31,13 +31,13 @@ class Date #:nodoc:
       mocked_time_stack_item = Timecop.top_stack_item
       mocked_time_stack_item.nil? ? nil : mocked_time_stack_item.date(self)
     end
-    
+
     alias_method :today_without_mock_date, :today
-    
+
     def today_with_mock_date
       mock_date || today_without_mock_date
     end
-    
+
     alias_method :today, :today_with_mock_date
   end
 end
@@ -48,26 +48,15 @@ class DateTime #:nodoc:
       mocked_time_stack_item = Timecop.top_stack_item
       mocked_time_stack_item.nil? ? nil : mocked_time_stack_item.datetime(self)
     end
-    
+
     def now_without_mock_time
       Time.now_without_mock_time.to_datetime
     end
-    
+
     def now_with_mock_time
       mock_time || now_without_mock_time
     end
 
     alias_method :now, :now_with_mock_time
-  end
-end
-
-# for ruby1.8
-unless Time::public_instance_methods.include? :to_datetime
-  class DateTime
-    class << self
-      def now_without_mock_time
-        Time.now_without_mock_time.send(:to_datetime)
-      end
-    end
   end
 end
