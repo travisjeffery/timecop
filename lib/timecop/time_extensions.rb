34a01ee7d5df954fa7f1bba93ee7925d38e7cd6b
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-12-05 09:47:11 +0000

    Provide correct defaults for Date::strptime
    Addresses concerns [raised][1] by @dmarkow in #109
    
    [1]: https://github.com/travisjeffery/timecop/pull/109#issuecomment-29726237

--- lib/timecop/time_extensions.rb
+++ lib/timecop/time_extensions.rb
@@ -43,7 +43,12 @@ class Date #:nodoc:
 
     alias_method :strptime_without_mock_date, :strptime
 
-    def strptime_with_mock_date(str, fmt)
+    def strptime_with_mock_date(str = '-4712-01-01', fmt = '%F', start = Date::ITALY)
+      unless start == Date::ITALY
+        raise ArgumentError, "Timecop's #{self}::#{__method__} only " +
+                             "supports Date::ITALY for the start argument."
+      end
+
       Time.strptime(str, fmt).to_date
     end
 
