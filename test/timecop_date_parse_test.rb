d216d29e123d0022a383880ca58b14a1097e099e
ballcheck <tim@audioboom.com>
2017-03-27 12:00:01 +0100

    removed test

--- test/timecop_date_parse_test.rb
+++ test/timecop_date_parse_test.rb
@@ -44,10 +44,6 @@ class TestTimecop < Minitest::Test
     assert_equal Date.parse("2008-09-06", false), Date.parse('Saturday')
   end
 
-  def test_date_parse_non_string_raises_type_error
-    assert_raises(TypeError) { Date.parse(Object.new) }
-  end
-
   def test_date_parse_nil_raises_type_error
     assert_raises(TypeError) { Date.parse(nil) }
   end
@@ -83,10 +79,6 @@ class TestTimecop < Minitest::Test
 
   def test_date_time_parse_with_additional_args
     assert_equal DateTime.parse("2008-09-06", false), DateTime.parse('Saturday')
-  end
-
-  def test_date_time_parse_non_string_raises_type_error
-    assert_raises(TypeError) { DateTime.parse(Object.new) }
   end
 
   def test_date_time_parse_nil_raises_type_error
