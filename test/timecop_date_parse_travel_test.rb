b846a3d68fe41700ea36f0d2e60f966007b64dae
Josh Cronemeyer <joshuacronemeyer@gmail.com>
2020-10-16 16:11:12 -0700

    Refactor date parse tests to remove the duplication among the 3 files. We are going to be adding a bunch more tests to fix lots of bugs with date parsing. It is difficult to remember to put the same test in multiple differnt places each time you add a scenario. Also it gets hard to track the duplication across the various classes. Lots of the date tests are identical to DateTime tests. I think a future refactor might be able to figure out how to reuse the Date tests and the DateTime tests can just focus on things that include time

---
+++ test/timecop_date_parse_travel_test.rb
@@ -0,0 +1,21 @@
+require_relative "test_helper"
+require 'timecop'
+require_relative 'date_parse_scenarios'
+require_relative 'date_time_parse_scenarios'
+
+class TestTimecop < Minitest::Test
+  def setup
+    t = Time.local(2008, 9, 1, 10, 5, 0) # monday
+    Timecop.travel(t)
+  end
+
+  def teardown
+    Timecop.return
+  end
+
+  # Test for Date
+  include DateParseScenarios
+
+  # Tests for DateTime
+  include DateTimeParseScenarios
+end
