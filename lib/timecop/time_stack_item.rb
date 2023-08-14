7e71d7ecc1666020e23d9303a93d99b106530b68
Josh <joshuacronemeyer@users.noreply.github.com>
2023-08-14 08:08:34 -0700

    Revert "Reduce memory by not duplicating an argument"
    This reverts commit 0ad9c6600b4f0f18fc484921ce8b3c9f5253fd18.

--- lib/timecop/time_stack_item.rb
+++ lib/timecop/time_stack_item.rb
@@ -56,7 +56,7 @@ class Timecop
 
     def time(time_klass = Time) #:nodoc:
       if @time.respond_to?(:in_time_zone)
-        time = time_klass.at(@time.localtime)
+        time = time_klass.at(@time.dup.localtime)
       else
         time = time_klass.at(@time)
       end
