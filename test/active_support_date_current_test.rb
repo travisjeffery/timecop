a4f13157761249350b6846f2173e9d067ec99442
Jerry Cheung <jollyjerry@gmail.com>
2024-08-14 10:08:48 -0700

    rm passing test
    Focus only on the failing test case with zone

--- test/active_support_date_current_test.rb
+++ test/active_support_date_current_test.rb
@@ -4,18 +4,6 @@ require 'active_support'
 require 'active_support/core_ext/date/calculations'
 
 class TestActiveSupportDateCurrent < Minitest::Test
-  def test_date_current_same_as_date_today
-    old_zone = Time.zone
-    begin
-      Time.zone = nil
-      Timecop.freeze("2024-03-15") do
-        assert_equal Date.current, Date.today
-      end
-    ensure
-      Time.zone = old_zone
-    end
-  end
-
   def test_date_current_same_as_date_today_with_time_zone
     old_zone = Time.zone
     begin
