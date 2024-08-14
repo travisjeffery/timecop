ce3e6bb232e7c8e817d496e1fe0f069ac417b611
Josh <joshuacronemeyer@users.noreply.github.com>
2024-08-14 09:40:23 -0700

    Revert "Calculate travel_offset to align with the precision of argument to Ti…" (#430)
    This reverts commit e05c2c07ba4ec93e84dcf228b4c617f98ad2489d.

--- lib/timecop/time_stack_item.rb
+++ lib/timecop/time_stack_item.rb
@@ -157,7 +157,7 @@ class Timecop
     end
 
     def compute_travel_offset
-      time.to_r - Time.now_without_mock_time.to_r
+      time - Time.now_without_mock_time
     end
 
     def times_are_equal_within_epsilon t1, t2, epsilon_in_seconds
