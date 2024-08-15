adf5ef7cdc784dfcf3533101d1b84af168d36af0
Jerry Cheung <jollyjerry@gmail.com>
2024-08-15 12:35:44 -0700

    debug ci

--- test/active_support_date_current_test.rb
+++ test/active_support_date_current_test.rb
@@ -5,10 +5,19 @@ require 'active_support/core_ext/date/calculations'
 
 class TestActiveSupportDateCurrent < Minitest::Test
   def test_date_current_same_as_date_today_with_time_zone
+    puts "Before Timecop.freeze"
+    puts "ENV['TZ']: #{ENV['TZ']}"
+    puts "Time.zone: #{Time.zone}"
+    puts
+
     old_zone = Time.zone
     begin
       Time.zone = "UTC"
       Timecop.freeze("2024-03-15") do
+        puts "Within Timecop.freeze"
+        puts "ENV['TZ']: #{ENV['TZ']}"
+        puts "Time.zone: #{Time.zone}"
+
         assert_equal Date.current, Date.today
       end
     ensure
