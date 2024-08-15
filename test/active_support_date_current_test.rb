dcdab1fbeea183575481b2fa05a03f1f4022d603
Jerry Cheung <jollyjerry@gmail.com>
2024-08-15 20:00:22 +0000

    set ENV['TZ'] system time to differ from Time.zone

--- test/active_support_date_current_test.rb
+++ test/active_support_date_current_test.rb
@@ -5,23 +5,11 @@ require 'active_support/core_ext/date/calculations'
 
 class TestActiveSupportDateCurrent < Minitest::Test
   def test_date_current_same_as_date_today_with_time_zone
-    puts "Before Timecop.freeze"
-    puts "ENV['TZ']: #{ENV['TZ']}"
-    puts "Time.zone: #{Time.zone}"
-    puts
-
-    old_zone = Time.zone
-    begin
+    each_timezone do
       Time.zone = "UTC"
       Timecop.freeze("2024-03-15") do
-        puts "Within Timecop.freeze"
-        puts "ENV['TZ']: #{ENV['TZ']}"
-        puts "Time.zone: #{Time.zone}"
-
         assert_equal Date.current, Date.today
       end
-    ensure
-      Time.zone = old_zone
     end
   end
 end
